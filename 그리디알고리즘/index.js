bdd7d79e022fb878bfed26d8e49482b5c14692f1
ksqrt <ksqrt98@gmail.com>
2022-09-25 12:08:18 +0000

    커밋테스트

---
+++ 그리디알고리즘/index.js
@@ -0,0 +1,21 @@
+
+// 1.어떤기능을하는지 2.뭘 넣고 뭘 뱉는지
+
+
+/**
+ * 
+ * @param {string} name 사람이름 넣으십쇼
+ * @param {number} age 나이 넣으십쇼
+ * @returns 두개 합쳐서 문자로 뱉어줌
+ */
+function hello(name,age) {
+    return name + age
+}
+
+hello("kim",20)
+
+
+/**
+ * 읽기전용이에요 수정하지 말아주세요
+ */
+const num =1;
