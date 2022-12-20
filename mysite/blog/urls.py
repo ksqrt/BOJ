cd96fab67bd7ad9d1a96d161313cc1474833c8d0
ksqrt <ksqrt98@gmail.com>
2022-12-20 00:11:27 +0000

    boj 4153

--- mysite/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('postlist', views.post_list, name='post_list'),
-
-]
--- mysite/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-
-def react(request):
-    return render(request, 'mysite/html/build/index.html', {})
